bd124434ad4d0d3e868b09918494c6ce24b59bd5
Dev3773 <101821030+Dev3773@users.noreply.github.com>
2024-11-13 20:10:29 +0000

    Deploying to gh-pages from @ Dev3773/goit-js-hw-09@9a7dc5b6ab2a5f3c5aebd1a2528eb65742270a5b 🚀

---
+++ 1-gallery.js
@@ -0,0 +1,2 @@
+import"./assets/styles-BibCt0QJ.js";const e=document.querySelector(".gallery__tittle");console.log(e);
+//# sourceMappingURL=1-gallery.js.map
---
+++ 2-form.js
@@ -0,0 +1,2 @@
+import"./assets/styles-BibCt0QJ.js";const o=document.querySelector(".form__tittle");console.log(o);
+//# sourceMappingURL=2-form.js.map
