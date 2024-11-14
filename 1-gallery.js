9a9229f7637be1de79b4ad702de10500f193c691
Dev3773 <101821030+Dev3773@users.noreply.github.com>
2024-11-14 18:00:50 +0000

    Deploying to gh-pages from @ Dev3773/goit-js-hw-09@812512cc8926cfa3c77ae841d0396e0041130c08 🚀

--- 1-gallery.js
+++ 1-gallery.js
@@ -1,2 +1,2 @@
-import"./assets/styles-D77nka9E.js";const e=document.querySelector(".gallery__tittle");console.log(e);
+import"./assets/modulepreload-polyfill-B5Qt9EMX.js";/* empty css                      */const e=document.querySelector(".gallery__tittle");console.log(e);
 //# sourceMappingURL=1-gallery.js.map
--- 2-form.js
+++ 2-form.js
@@ -1,2 +1,2 @@
-import"./assets/styles-D77nka9E.js";const o=document.querySelector(".form__tittle");console.log(o);
+import"./assets/modulepreload-polyfill-B5Qt9EMX.js";/* empty css                      */const o=document.querySelector(".form__tittle");console.log(o);
 //# sourceMappingURL=2-form.js.map
