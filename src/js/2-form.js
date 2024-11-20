16d55e0752eabbc6df67bf4316309feb2a6c304c
Dev3773 <stepaniuk.sergiy@gmail.com>
2024-11-21 00:13:18 +0200

    Made task two, form

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -1,11 +1,44 @@
-const inputRef = document.querySelector(".form__input");
+const LOCAL_STORAGE_KEY = "feedback-form-state";
+const formRef = document.querySelector(".feedback-form");
 
-const fooRef = (event) => {
-  if (event.target.value) {
-     inputRef.classList.add('active');
+const formData = {email: "", message: ""};
+
+const savedData = localStorage.getItem(LOCAL_STORAGE_KEY);
+const parsedData = JSON.parse(savedData);
+
+if (savedData) {
+  formData.email = parsedData.email || "";
+  formData.message = parsedData.message || "";
+  
+  formRef.elements.email.value = formData.email;
+  formRef.elements.message.value = formData.message;
+}
+
+const formHandler = (evt) => {
+  if (evt.target.name === "email") {
+    formData.email = evt.target.value.trim();
+  }
+  else if (evt.target.name === "message") {
+    formData.message = evt.target.value.trim();
+  }
+
+  localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(formData));
+}
+  
+const listenSubmit = (evt) => {
+  evt.preventDefault();
+
+  const inputRef = evt.target.elements.email.value;
+  const textareaRef = evt.target.elements.message.value
+
+  if (inputRef === ""|| textareaRef === "") {
+    return alert("Fill please all fields");
   } else {
-  inputRef.classList.remove('active');
-   }
-} 
+    console.log(formData);
+}
+    localStorage.removeItem(LOCAL_STORAGE_KEY);
+    formRef.reset();
+};
 
-  inputRef.addEventListener('input', fooRef);
+formRef.addEventListener("input", formHandler);
+formRef.addEventListener("submit", listenSubmit);
